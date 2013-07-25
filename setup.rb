4d7b2a96252d58820c5193c6e5e37f1afddaaf6b
pikesley <sam.pikesley@theodi.org>
2013-07-25 12:18:09 +0000

    Setup now much more comprehensive

--- setup.rb
+++ setup.rb
@@ -33,27 +33,54 @@ puts green "We're going to grab all the actual applications we need."
 
 pwd = `pwd`.strip
 
+port = 9000
 projects.each_pair do |project, servername|
-
   if not Dir.exists? project.to_s
-
     puts "%s %s" % [
       green("Cloning"),
       red(project)
     ]
-
     system "git clone git@github.com:#{organisation}/#{project}.git"
-    system "rvm in #{project} do bundle"
+  else
+    puts "%s %s" % [
+      green("Updating"),
+      red(project)
+    ]
+    system "cd #{project} && git pull && cd ../"
   end    
+  
+  puts "%s %s" % [
+    green("Bundling"),
+    red(project)
+  ]
+
+  system "rvm in #{project} do bundle"
+  system "ln -sf env #{project}/.env"
+  if File.exists? "%s/Procfile" % [
+    project
+  ]
+
+    puts "%s %s" % [
+      green("Generating upstart scripts for"),
+      red(project)
+    ]
+   
+    Dir.chdir project.to_s do
+      command = "rvm in . do rvmsudo bundle exec foreman export -a %s -u %s -p %d upstart /etc/init" % [
+        project,
+        `whoami`.strip,
+        port
+      ]
+      system command
+    end
+  end
+
+  port += 1000
 end
 
-exit
+#system "ln -sf #{pwd}/frontend ~/.pow/private-frontend"
 
-system "ln -sf #{pwd}/frontend ~/.pow/private-frontend"
-
-puts "\x1B[32m"
-puts "Now we need to generate application tokens in the signonotron."
-puts "\x1B[0m"
+puts green "Now we need to generate application tokens in the signonotron."
 
 def oauth_id(output)
   output.match(/config.oauth_id     = '(.*?)'/)[1]
@@ -66,51 +93,45 @@ end
 Dir.chdir("signonotron2") do
   RVM.use! '.'
 
-  puts "\x1B[32m"
-  puts "Setting up signonotron database..."
-  puts "\x1B[0m"
+  puts green "Setting up signonotron database..."
 
-  system "mysqladmin -u root create signonotron2_development"
+  system "mysqladmin -u root create signonotron2"
   system "mysql -u root < ../db_setup.sql"
 
-  system "rake db:schema:load"
+  system "RACK_ENV=production rake db:schema:load"
   
-  puts "\x1B[32m"
-  puts "Make signonotron work in dev mode..."
-  puts "\x1B[0m"
+  puts green "Make signonotron work in dev mode..."
 
   system "bundle exec ./script/make_oauth_work_in_dev"
   
-  puts "\x1B[32m"
-  puts "Generating application keys for \x1B[31mpublisher\x1B[0m"
-  puts "\x1B[0m"
+  puts "%s %s" % [
+    green("Generating application keys for"),
+    red("publisher")
+  ]
 
   str = `rake applications:create name=Publisher description="Content editing" home_uri="http://publisher.dev" redirect_uri="http://publisher.dev/auth/gds/callback"`
-  File.open('../publisher/.powenv', 'a') do |f|
+  File.open('../env', 'a') do |f|
     f << "export PUBLISHER_OAUTH_ID=#{oauth_id(str)}\n"
     f << "export PUBLISHER_OAUTH_SECRET=#{oauth_secret(str)}\n"
   end
   
-  puts "\x1B[32m"
-  puts "Generating application keys for \x1B[31mpanopticon\x1B[0m"
-  puts "\x1B[0m"
+  puts "%s %s" % [
+    green("Generating application keys for"),
+    red("panopticon")
+  ]
 
   str = `rake applications:create name=Panopticon description="Metadata management" home_uri="http://panopticon.dev" redirect_uri="http://panopticon.dev/auth/gds/callback"`
-  File.open('../panopticon/.powenv', 'a') do |f|
+  File.open('../env', 'a') do |f|
     f << "export PANOPTICON_OAUTH_ID=#{oauth_id(str)}\n"
     f << "export PANOPTICON_OAUTH_SECRET=#{oauth_secret(str)}\n"
   end
   
-  puts "\x1B[32m"
-  puts "We'll generate a couple of sample users for you. You can add more by doing something like:"
-  puts "\x1B[31m"
-  puts "$ cd signonotron2"
-  puts "$ rvm use .."
-  puts "$ bundle exec rake users:create name='Alice' email=alice@example.com applications=Publisher,Panopticon"
-  puts "\x1B[0m"
+  puts green "We'll generate a couple of sample users for you. You can add more by doing something like:"
+  puts red "$ cd signonotron2"
+  puts red "$ rvm use ."
+  puts red "$ bundle exec rake users:create name='Alice' email=alice@example.com applications=Publisher,Panopticon"
 
   system "GOVUK_APP_DOMAIN=dev DEV_DOMAIN=dev bundle exec rake users:create name='Alice' email=alice@example.com applications=Publisher,Panopticon"
   system "GOVUK_APP_DOMAIN=dev DEV_DOMAIN=dev bundle exec rake users:create name='Bob' email=bob@example.com applications=Publisher,Panopticon"
-
 end
 
